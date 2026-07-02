224d8a5fea00e241a712a37837234ab36118a5f2
Wei FeiYang <wesley.f.young@outlook.com>
2026-07-02 17:21:07 +0800

    Implement face reaction plugin

--- README.md
+++ README.md
@@ -1,7 +1,34 @@
-# fraq-plugin-template
+# fraq-plugin-face-reaction
 
-用于开发 Fraq 插件的模板仓库。包含了：
+让机器人回应（贴）任何表情。
 
-- TypeScript 配置（`tsconfig.json`）
-- Biome 配置（`biome.json`），用于代码格式化和 linting
-- 简单的测试脚本，可以通过 `npx tsx test/smoke.ts` 运行
+## 安装与配置
+
+将插件添加至 `dependencies`，然后在创建 `Context` 时引入并配置插件：
+
+```typescript
+import FaceReactionPlugin from "fraq-plugin-face-reaction";
+
+ctx.install(FaceReactionPlugin);
+```
+
+## Usage
+
+发送 `react` 命令即可让机器人给当前消息贴上指定的表情：
+
+```
+react <一个 Emoji 或 QQ 表情>
+```
+
+机器人将会给当前消息贴上指定的表情。也可以通过表情 ID 来贴表情：
+
+```
+react <face|emoji> <表情 ID>
+```
+
+此外，也可以引用一条消息再发送 `react` 命令来让机器人给引用的消息贴上指定的表情：
+
+```
+[引用消息] react <一个 Emoji 或 QQ 表情>
+[引用消息] react <face|emoji> <表情 ID>
+```
--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,96 @@
 import { definePlugin, param } from '@fraqjs/fraq';
 
-export const ExamplePlugin = definePlugin({
-  name: 'example-plugin', // Change this to your plugin's name
+export const FaceReactionPlugin = definePlugin({
+  name: 'face-reaction',
   apply(ctx) {
-    // Start coding here!
-    ctx.router
-      .command('echo')
-      .arg('content', param.str())
-      .execute((session, { content }) => {
-        session.reply(`You said: ${content}`);
+    const router = ctx.router.filter((session) => session.raw.message_scene === 'group');
+
+    // react emoji/face to current message
+    const reactCmd = router.group('react');
+    reactCmd
+      .rawPattern()
+      .arg('reaction', param.str())
+      .execute(async (session, { reaction }) => {
+        const reactionId = reaction.codePointAt(0);
+        if (!reactionId || reactionId < 0xff) return; // is an ASCII character, not a valid emoji
+        await session.reaction('emoji', reactionId.toString());
+      });
+    reactCmd
+      .rawPattern()
+      .arg('reaction', param.segment('face'))
+      .execute(async (session, { reaction }) => {
+        await session.reaction('face', reaction.data.face_id);
+      });
+    reactCmd
+      .rawPattern()
+      .arg('type', param.union('emoji', 'face'))
+      .arg('reaction', param.str())
+      .execute(async (session, { type, reaction }) => {
+        if (type === 'emoji') {
+          const reactionId = reaction.codePointAt(0);
+          if (!reactionId || reactionId < 0xff) return; // is an ASCII character, not a valid emoji
+          await session.reaction('emoji', reactionId.toString());
+        } else if (type === 'face') {
+          await session.reaction('face', reaction);
+        }
+      });
+
+    // react emoji/face to a referenced message
+    router
+      .rawPattern()
+      .arg('reference', param.segment('reply'))
+      .arg('literal', param.literal('react'))
+      .arg('reaction', param.str())
+      .execute(async (session, { reference, reaction }) => {
+        const reactionId = reaction.codePointAt(0);
+        if (!reactionId || reactionId < 0xff) return; // is an ASCII character, not a valid emoji
+        await ctx.client.send_group_message_reaction({
+          group_id: session.raw.peer_id,
+          message_seq: reference.data.message_seq,
+          reaction_type: 'emoji',
+          reaction: reactionId.toString(),
+        });
+      });
+    router
+      .rawPattern()
+      .arg('reference', param.segment('reply'))
+      .arg('literal', param.literal('react'))
+      .arg('reaction', param.segment('face'))
+      .execute(async (session, { reference, reaction }) => {
+        await ctx.client.send_group_message_reaction({
+          group_id: session.raw.peer_id,
+          message_seq: reference.data.message_seq,
+          reaction_type: 'face',
+          reaction: reaction.data.face_id,
+        });
+      });
+
+    router
+      .rawPattern()
+      .arg('reference', param.segment('reply'))
+      .arg('literal', param.literal('react'))
+      .arg('type', param.union('emoji', 'face'))
+      .arg('reaction', param.str())
+      .execute(async (session, { reference, type, reaction }) => {
+        if (type === 'emoji') {
+          const reactionId = reaction.codePointAt(0);
+          if (!reactionId || reactionId < 0xff) return; // is an ASCII character, not a valid emoji
+          await ctx.client.send_group_message_reaction({
+            group_id: session.raw.peer_id,
+            message_seq: reference.data.message_seq,
+            reaction_type: 'emoji',
+            reaction: reactionId.toString(),
+          });
+        } else if (type === 'face') {
+          await ctx.client.send_group_message_reaction({
+            group_id: session.raw.peer_id,
+            message_seq: reference.data.message_seq,
+            reaction_type: 'face',
+            reaction: reaction,
+          });
+        }
       });
   },
 });
 
-export default ExamplePlugin; // Also export the plugin as default
+export default FaceReactionPlugin;
